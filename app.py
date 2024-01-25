f8adba8b3f52e6b2474739fa9d93b0361cdce9d8
Guest_Admin <guest_admin@Ambers-MacBook-Pro.local>
2024-01-25 16:19:12 +0100

    added a while loop to choose to continue or end the game

--- app.py
+++ app.py
@@ -2,41 +2,39 @@ import random
 
 choices = ["rock", "paper", "scissors"]
 
-print("\nWelcome to the game: Rock, Paper, Scissors!\n")
-print("Here are the rules of the game:")
-print("""
-    Rock wins over scissors
-    Scissors wins over paper
-    Paper wins over rock
-    If both the computer and the player choose the same option, the result is a tie.")
-""")
-
 while True:
-    user_play = input("Please select (rock, paper, scissors):").lower()
+    print("\nWelcome to the game: Rock, Paper, Scissors!\n")
+    print("Here are the rules of the game:")
+    print("""
+        Rock wins over scissors
+        Scissors wins over paper
+        Paper wins over rock
+        If both the computer and the player choose the same option, the result is a tie.
+    """)
 
+    while True:
+        user_play = input("Please select (rock, paper, scissors):").lower()
 
-    if user_play not in choices:
-        print("Invalid selection! Select (rock, paper, scissors)")
+        if user_play not in choices:
+            print("Invalid selection! Select (rock, paper, scissors)")
+        else:
+            print(f"You selected: {user_play}")
+            break
+
+    computer_play = random.choice(choices)
+    print(f"The computer chose: {computer_play}")
+
+    if user_play == computer_play:
+        print("It's a tie!") 
+    elif (
+        (user_play == 'rock' and computer_play == 'scissors') or
+        (user_play == 'paper' and computer_play == 'rock') or
+        (user_play == 'scissors' and computer_play == 'paper')
+    ):
+        print("Player wins!") 
     else:
-     print(f"You selected: {user_play}")
-     break
+        print("Computer wins!") 
 
-computer_play = random.choice(choices)
-print(f"The computer chose: {computer_play}")
-
-if user_play == computer_play:
-   print("It's a tie!") 
-elif (
-   (user_play == 'rock' and computer_play == 'scissors') or
-   (user_play == 'paper' and computer_play == 'rock') or
-   (user_play == 'scissors' and computer_play == 'paper')
-):
-   print("Player wins!") 
-else:
-   print("Computer wins!") 
-
-print(f"\nWould you like to play again? Y/N")
-
-   
-
-
+    play_again = input("\nWould you like to play again? Y/N: ").upper()
+    if play_again != 'Y':
+        break
